c6993f37e5fd43d861f27909a428fdaaf1368b59
Nick Crumb <nicholcr@umich.edu>
2021-12-30 22:30:18 -0500

    added navigation tests
    with the nav graph

--- app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
+++ app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -1,6 +1,10 @@
 package com.example.wordsapp
 
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -8,6 +12,7 @@ import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -32,5 +37,28 @@ class NavigationTests {
                 .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
 
         onView(withText("Words That Start With C")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun navigate_to_words_nav_component() {
+
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+        val letterListScenario = launchFragmentInContainer<LetterListFragment>(
+            themeResId = R.style.Theme_Words
+        )
+
+        letterListScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        onView(withId(R.id.recycler_view))
+            .perform(RecyclerViewActions
+                .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
+
+        assertEquals(navController.currentDestination?.id, R.id.wordListFragment)
     }
 }
